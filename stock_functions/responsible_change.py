8ba9222a6fd6cc0eba147d472a9d966b09812afa
Anibal Arenas <anibal.p@alam.mx>
2025-04-20 21:09:16 -0600

    [FIX] Fix responsible change

--- stock_functions/responsible_change.py
+++ stock_functions/responsible_change.py
@@ -22,16 +22,14 @@ import pandas as pd
 import datetime
 
 print('================================================================')
-print('BIENVENIDO AL PROCESO DE FACTURACIÓN WALMART')
-print('================================================================')
-print('SCRIPT DE CREACIÓN DE FACTURAS GLOBALES')
+print('CAMBIAR RESPONSABLE')
 print('================================================================')
 today_date = datetime.datetime.now()
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print('Fecha:' + today_date.strftime("%Y-%m-%d %H:%M:%S"))
 #Archivo de configuración - Use config_dev.json si está haciendo pruebas
 #Archivo de configuración - Use config.json cuando los cambios vayan a producción
-config_file_name = r'C:\Dev\wb_odoo_external_api\config\config.json'
+config_file_name = r'E:\Dev\odoo_external_api\config\config.json'
 
 def get_odoo_access():
     with open(config_file_name, 'r') as config_file:
@@ -55,7 +53,7 @@ def change_responsible_id():
     print('Conexión con Odoo establecida')
     print('----------------------------------------------------------------')
     print('Leyendo archivo')
-    excel_file_path = r'C:\dev\odoo_external_api\stock_functions\files\responsible_id.xlsx'
+    excel_file_path = r'E:\Dev\odoo_external_api\stock_functions\files\responsible_id.xlsx'
     sale_file = pd.read_excel(excel_file_path, usecols=['ID'])
     product_ids = sale_file['ID'].tolist()
     progress_bar = tqdm(total=len(product_ids), desc="Procesando")
@@ -64,20 +62,26 @@ def change_responsible_id():
         print('----------------------------------------------------------------')
         # Consultamos a sale.order para obtener los campos requeridos de cada orden de venta
         for each in product_ids:
-            domain = ['id', '=', each]
-            products = models.execute_kw(db_name, uid, password, 'product.template', 'search_read', [[domain]])[0]
+            products = models.execute_kw(db_name, uid, password, 'product.product', 'search_read', [[['id', '=', each]]])[0]
             if products:
                 product_id = products['id']
-                responsible = products['responsible_id']['name']
-                upd_responsible = models.execute_kw(db_name, uid, password, 'product_template', 'write', [[product_id], {'responsible_id': 1936}])
-                check_responsible = products = models.execute_kw(db_name, uid, password, 'product.template', 'search_read', [[['id', '=', product_id]]])[0]
-                check_name = check_responsible['responsible_id']['name']
+                product_tmpl = products['product_tmpl_id'][0]
+                if product_id:
+                    product_tmpl_id = models.execute_kw(db_name, uid, password, 'product.template', 'search_read', [[['id', '=', product_tmpl]]])[0]
+                    responsible = product_tmpl_id['responsible_id'][0]
+                    responsible_name = product_tmpl_id['responsible_id'][1]
+                    upd_responsible = models.execute_kw(db_name, uid, password, 'product.template', 'write', [[product_tmpl], {'responsible_id': 1936}])
+                    check_responsible = products = models.execute_kw(db_name, uid, password, 'product.template', 'search_read', [[['id', '=', product_tmpl]]])[0]
+                    check_name = check_responsible['responsible_id'][1]
 
-                message = {
-                    'body': f'El responsable de este producto fue cambiado vía API por el área de IT de: {responsible} a {check_name}',
-                    'message_type': 'comment',
-                }
-                write_msg_tech = models.execute_kw(db_name, uid, password, 'product.template', 'message_post', [product_id], message)
+                    message = {
+                        'body': f'El responsable de este producto fue cambiado vía API por el área de IT de: {responsible_name} a {check_name}',
+                        'message_type': 'comment',
+                    }
+                    write_msg_tech = models.execute_kw(db_name, uid, password, 'product.template', 'message_post', [product_tmpl], message)
+                else:
+                    print(f"No existe plantilla de producto que corresponda a {product_id}")
+                    continue
             else:
                 print(f"No existe un producto que corresponda a {each}")
                 continue
